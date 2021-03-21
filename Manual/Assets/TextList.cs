8c024a1b1a4fa02a010c97edb6fb4438d438d46c
Fartlicker777 <62307787+Fartlicker777@users.noreply.github.com>
2021-03-21 02:27:49 -0500

    Fixed the mod

--- Manual/Assets/TextList.cs
+++ Manual/Assets/TextList.cs
@@ -4,7 +4,7 @@ public static class TextList
     {
         //Page 1
         "On the Subject of Roger",
-        "\nThe module is not here, however\nit has been sent to your expert.\nHelp the expert through the\nmodule so you can get rid of this manual to\nsee the status light. Please tell your\nexpert to input the number below into the\ntop-right display of the module to begin,\nthen follow the steps on the rest of the\npages in the order they are given."
+        "I am fat."
         //Shape Pressing Step
         
     };
